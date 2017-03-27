65420075277f7bb3ba2273651ae4f99568d1adcd
Ryan Chin <ryanchin@me.com>
2017-03-27 18:38:16 -0400

    u wot m8

---
+++ Assets/scripts/HitboxY.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxY : MonoBehaviour {
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 1F;
+    public float sensitivityY = 1F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rotationY += Input.GetAxis("Mouse Y") * 1f;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        //
+    }
+}
---
+++ Assets/scripts/Interactions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions : MonoBehaviour {
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "key" && Input.GetMouseButton(0))
+        {
+            other.transform.position = gameObject.transform.position;
+        }
+    }
+}
