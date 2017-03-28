f4d99b11a0db922e5c48dddaaccfbb7f9cffe42d
Daniel Jiang <djj286@nyu.edu>
2017-03-27 20:52:30 -0400

    Add files via upload
    sample code for moving platforms, make sure both the platform and player are rigidbodies

---
+++ daniel/movingplatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingplatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time, 3), transform.position.z);
+	}
+}
