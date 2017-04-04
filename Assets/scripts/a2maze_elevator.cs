4d5bebca312422b454588f892068078e4113ff8b
Daniel Jiang <djj286@nyu.edu>
2017-04-04 14:26:28 -0400

    prelim assets for awe

---
+++ Assets/scripts/a2maze_elevator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class a2maze_elevator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.y = Mathf.PingPong(Time.time * 6, 30) + 87 ;
+		transform.position = pos;
+	}
+}
