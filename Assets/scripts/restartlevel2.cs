aaa3822c801d53fe0c3be6e6ac155be8054e18c1
Daniel Jiang <djj286@nyu.edu>
2017-04-11 00:50:58 -0400

    tidying
    furniture a2 and implemented restart level 2

---
+++ Assets/scripts/restartlevel2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartlevel2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+}
