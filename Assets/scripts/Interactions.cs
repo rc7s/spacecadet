c631b75b09a0621eeadd86131d1fd0a4d0675600
Ryan Chen <chenr@172-16-18-140.DYNAPOOL.NYU.EDU>
2017-04-18 14:01:20 -0400

    update
    chickens

--- Assets/scripts/Interactions.cs
+++ Assets/scripts/Interactions.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour {
     public Rigidbody rb;
+	public bool pickup;
     // Use this for initialization
     void Start () {
-		
+		pickup = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-    
 	}
 
     void OnTriggerStay(Collider other)
     {
-        if (other.tag == "key" && Input.GetMouseButton(0))
+        if (other.tag == "key")
         {
-            other.transform.position = gameObject.transform.position;
+			if (Input.GetMouseButtonDown (0)) {
+				if (pickup = false) {
+					pickup = true;
+				}
+				if (pickup = true) {
+					pickup = false;
+				}
+			}
+			if (pickup = true) {
+				other.transform.position = gameObject.transform.position;
+				if (Input.GetMouseButtonDown (0)) {
+					pickup = false;
+				}
+			}
         }
     }
 }
