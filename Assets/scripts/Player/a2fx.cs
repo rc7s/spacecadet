8332074456055754eb209baa4554903541586777
Ryan Chin <ryanchin@me.com>
2017-04-25 16:31:34 -0400

    lsd
    ye

---
+++ Assets/scripts/Player/a2fx.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+public class a2fx : MonoBehaviour
+{
+    public VignetteAndChromaticAberration chromatic_Vignette;
+    public Twirl twirl;
+    public ColorCorrectionCurves curves;
+    // Use this for initialization
+    void Start()
+    {
+        chromatic_Vignette = GetComponent<VignetteAndChromaticAberration>();
+        twirl = GetComponent<Twirl>();
+        curves = GetComponent<ColorCorrectionCurves>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chromatic_Vignette.chromaticAberration = 10 + Mathf.PingPong(Time.time*10, 26);
+        twirl.angle = Mathf.PingPong(Time.time * 3, 5);
+        curves.saturation = 0.8f + Mathf.PingPong(Time.time, 2);
+    }
+}
