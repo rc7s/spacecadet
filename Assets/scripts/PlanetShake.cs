ecc24895038f48fe6bb974167e0bd506910057e8
Ryan Chin <ryanchin@me.com>
2017-04-25 02:06:09 -0400

    changes
    ye

---
+++ Assets/scripts/PlanetShake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetShake : MonoBehaviour {
+    public Vector3 shake;
+	// Use this for initialization
+	void Start () {
+        shake.x = 2f;
+        shake.y = 2f;
+        shake.z = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        iTween.ShakeScale(gameObject,shake, 5f);
+
+    }
+}
