c6c7206d90df581021a7a4ac56332215a0b246c5
Ryan Chin <ryanchin@me.com>
2017-04-25 01:22:44 -0400

    lvl3
    marcopolo

---
+++ Assets/scripts/a3/level3_marcopolo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class level3_marcopolo : MonoBehaviour {
+    public Transform other;
+    public Transform player;
+    public float dist;
+    public GameObject go;
+
+    // Use this for initialization
+    void Start () {
+        go = GameObject.Find("marcopolo");
+        dist = 999999f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		dist = Vector3.Distance(other.position, player.position);
+        go.GetComponent<AudioSource>().volume = 0.5f;
+    }
+}
