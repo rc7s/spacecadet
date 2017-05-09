fafb802946b85b75d9067d0913ad5ef414b0fce1
Daniel Jiang <djj286@nyu.edu>
2017-05-08 22:28:48 -0400

    completed chickens but my comp cant run

--- Assets/scripts/chickenmove.cs
+++ Assets/scripts/chickenmove.cs
@@ -5,63 +5,70 @@ using UnityEngine;
 public class chickenmove : MonoBehaviour {
 	public float ms;
 	public float timer;
+	public float timer2;
 	public float rotateTimer;
 	public float smooth = 1f;
 	private Vector3 targetAngles;
 	public bool forward;
 	public bool backwards; 
 	public Rigidbody rb;
+	public float currentHeight;
+	public bool loop;
 
 	// Use this for initialization
 	void Start () {
 		ms = 1.0f;
-		timer = 4.0f;
-		rotateTimer = 2.0f;
 		forward = true;
 		backwards = false;
+		loop = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		timer -= Time.deltaTime;
+		//currentHeight = transform.position.y;
 
-		if (timer >= 0) {
-			if (forward) {
-				transform.position += transform.forward * ms / 2;
+		while (loop) {
+			timer = 2.0f;
+			rotateTimer = 2.0f;
+			timer2 = 2.0f;
+
+			if (timer >= 0) {
+				timer -= Time.deltaTime;
+
+				//if (forward) {
+					transform.position += transform.forward * ms / 2;
+				//}
+
 			}
-			if (backwards) {
+
+			if (timer <= 0) {
+				rotateTimer -= Time.deltaTime;
+				targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
+				transform.eulerAngles = Vector3.Lerp (transform.eulerAngles, targetAngles, smooth * Time.deltaTime / 2); // lerp to new angles
+				/*if (rotateTimer <= 0) {
+					timer = 4.0f;
+					if (forward) {
+						forward = false;
+						backwards = true;
+					}
+					if (backwards) {
+						backwards = false;
+						forward = true;
+					}
+				}*/
+			}
+
+			if (timer2 < 0) {
+				timer2 -= Time.deltaTime;
 				transform.position -= transform.forward * ms / 2;
 			}
 		}
-
-		if (timer <= 0) {
-			rotateTimer -= Time.deltaTime;
-			targetAngles = transform.eulerAngles + 180f * Vector3.up; // what the new angles should be
-			transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, targetAngles, smooth * Time.deltaTime); // lerp to new angles
-			if (forward) {
-				forward = false;
-				backwards = true;
-			}
-			if (backwards) {
-				backwards = false;
-				forward = true;
-			}
-			if (rotateTimer <= 0) {
-				timer = 4.0f;
-			}
-		}
 	}
 
-	void OnCollisionStay(Collider other){
+	/*void OnCollisionEnter(Collider other){
 		if (other.tag == "floor") {
-			rb.useGravity = false;
+			transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 		}
-	}
-
-	void OnCollisionExit(Collider other){
-		if (other.tag == "floor"){
-			rb.useGravity = true;
-		}
-	}
+	}*/
 }
